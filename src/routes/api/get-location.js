ccdcc80b1ed8253c7fe7fca6167af8253b659de7
Diego Artiles <diego45882@hotmail.com>
2022-06-02 16:00:20 -0300

    chore: restore api location

--- src/routes/api/get-location.js
+++ src/routes/api/get-location.js
@@ -8,15 +8,8 @@ export async function get() {
 	};
 	
 	
-	// const location = await fetch('https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation?apikey=873dbe322aea47f89dcf729dcc8f60e8', options)
-	// 	.then(res => res.json())
-
-	const location = {
-		ip: '190.195.10.159',
-		longitude: -58.3845,
-		latitude: -34.6021
-
-	}
+	const location = await fetch('https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation?apikey=873dbe322aea47f89dcf729dcc8f60e8', options)
+		.then(res => res.json())
 
 	return {
 		status: 200,
